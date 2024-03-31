f246d605b36fa3a03a3bc1701230da15c3939fa4
kirillmukh <kirillmukh@mail.ru>
2024-03-31 18:27:24 +0300

    Initial commit

---
+++ src/main/java/com/kirillmukh/dictionary/DictionaryBotApplication.java
@@ -0,0 +1,13 @@
+package com.kirillmukh.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirillmukh/dictionary/DictionaryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirillmukh.dictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
